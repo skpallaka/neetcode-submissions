565622f64d78ca801588559ce5b6eabd807f305c
skpallaka <skushalpallaka@gmail.com>
2026-08-21 13:24:41 -0700

    Add: two-integer-sum - submission-19

---
+++ Data Structures & Algorithms/two-integer-sum/submission-19.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in my_dict:
+                return [my_dict[complement],i]
+            else:
+                my_dict[nums[i]] = i 
+        
+
+    '''
+    3,4,5,6
+    complement = 7-3 =4
+    else:
+        my dict[3] = 0  3 is key 0 is val
+7-4 = 3
+so in mydict so then returns 0,1
+    '''
+
+            
