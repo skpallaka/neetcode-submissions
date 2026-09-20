57b4fbb058c84eee51b8accd42a09b909379819c
skpallaka <skushalpallaka@gmail.com>
2026-09-19 20:39:22 -0700

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    # 1) count each string in the list
+    # 2) set a delimter in between to show diff
+    # 3) combine and smush and then send to decode function
+    # s is string in strs: len s  + delimter +s , repeat
+
+    def encode(self, strs: List[str]) -> str:
+        smush = []
+        for s in strs:
+            len_s = len(s)
+            combined = str(len_s) + "-" + s  # typeerror
+            smush.append(combined)
+        return "".join(smush)
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        i = 0
+        while i < len(s):
+            fnd = s.find("-", i)
+            length = int(s[i:fnd])
+            start = fnd + 1
+            end = start + length
+            result.append(s[start:end])
+            i = end
+        return result
